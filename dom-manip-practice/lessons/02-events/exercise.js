423bcbeb3a1be3e7ccbf15299d85f81743dc141f
Mattias Eskilsson <mattiaseskilsson@hotmail.se>
2025-10-18 17:27:06 +0200

    02 events done

--- dom-manip-practice/lessons/02-events/exercise.js
+++ dom-manip-practice/lessons/02-events/exercise.js
@@ -3,3 +3,66 @@
 // - Use ONE click listener on #todo-list to handle both: toggle complete & delete (delegation).
 // - Update the counts after every change.
 // - Bonus: save todos in localStorage and load on start.
+
+
+
+
+let formSubmit = document.getElementById('todo-form');
+let todoList = document.getElementById('todo-list');
+let todoInput = document.getElementById('todo-input');
+let counts = document.getElementById('counts');
+
+todoList.innerHTML = localStorage.getItem('todoList') || "";
+
+
+updateCount();
+
+formSubmit.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    let newLi = document.createElement('li');
+    let btnDelete = document.createElement('button');
+    let checkbox = document.createElement('input');
+    let text = document.createElement('p');
+
+
+    btnDelete.textContent = "Delete";
+    text.textContent = todoInput.value;
+    checkbox.type = "checkbox";
+
+
+
+    newLi.appendChild(text);
+    newLi.appendChild(checkbox);
+    newLi.appendChild(btnDelete);
+    todoList.appendChild(newLi);
+    updateCount();
+    localStorage.setItem('todoList', todoList.innerHTML);
+});
+
+todoList.addEventListener('click', (e) => {
+    if (e.target.matches('button')) {
+        e.target.parentElement.remove();
+    }
+    if (e.target.matches('input[type="checkbox"]')) {
+        e.target.closest('li').classList.toggle('completed');
+    }
+    updateCount();
+    localStorage.setItem('todoList', todoList.innerHTML);
+});
+
+function updateCount() {
+    let total = 0;
+    let done = 0;
+    let left = 0;
+
+    total = todoList.querySelectorAll('li').length;
+    done = todoList.querySelectorAll('li.completed').length;
+    left = total - done;
+
+    counts.textContent = `${total} total • ${done} done • ${left} left`;
+}
+
+
+localStorage.setItem('todoList', todoList.innerHTML);
